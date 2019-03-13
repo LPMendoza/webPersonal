5078aa990a20614865bd6293d626ef7774a96813
LPMendoza <47892535+LPMendoza@users.noreply.github.com>
2019-03-12 22:41:59 -0600

    Web Personal
    Efecto al menu

--- js/main.js
+++ js/main.js
@@ -9,4 +9,20 @@ window.onscroll = function () {
       menuBar.style.background = 'rgba(0, 0, 0, 0)';
       menuBar.style.boxShadow = 'none';
    }
+
 };
+
+window.onload = function () {
+
+   let menuBar = document.querySelector('#menuBar');
+
+   if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+      menuBar.style.background = '#536dfe';
+      menuBar.style.boxShadow = '0px 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23)';
+   } else {
+      menuBar.style.background = 'rgba(0, 0, 0, 0)';
+      menuBar.style.boxShadow = 'none';
+   }
+   
+};
+
